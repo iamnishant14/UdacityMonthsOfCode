944ab340da1c1895f2bfb29c2ffb24788df28b55
Nishant <nishantnks@hotmail.com>
2018-03-17 00:47:40 +0530

    INIT commit for T04b.01-OpenWebPage(COMPLETED)

--- app/src/main/java/com/example/android/implicitintents/MainActivity.java
+++ app/src/main/java/com/example/android/implicitintents/MainActivity.java
@@ -15,10 +15,14 @@
  */
 package com.example.android.implicitintents;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
+
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,10 +39,10 @@ public class MainActivity extends AppCompatActivity {
      * @param v Button that was clicked.
      */
     public void onClickOpenWebpageButton(View v) {
-        // TODO (5) Create a String that contains a URL ( make sure it starts with http:// or https:// )
-
-        // TODO (6) Replace the Toast with a call to openWebPage, passing in the URL String from the previous step
-        Toast.makeText(this, "TODO: Open a web page when this button is clicked", Toast.LENGTH_SHORT).show();
+        // COMPLETED (5) Create a String that contains a URL ( make sure it starts with http:// or https:// )
+        String url="https://www.udacity.com";
+        // COMPLETED (6) Replace the Toast with a call to openWebPage, passing in the URL String from the previous step
+        openWebPage(url);
     }
 
     /**
@@ -48,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
      * @param v Button that was clicked.
      */
     public void onClickOpenAddressButton(View v) {
-        Toast.makeText(this, "TODO: Open a map when this button is clicked", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "COMPLETED: Open a map when this button is clicked", Toast.LENGTH_SHORT).show();
     }
 
     /**
@@ -58,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
      * @param v Button that was clicked.
      */
     public void onClickShareTextButton(View v) {
-        Toast.makeText(this, "TODO: Share text when this is clicked", Toast.LENGTH_LONG).show();
+        Toast.makeText(this, "COMPLETED: Share text when this is clicked", Toast.LENGTH_LONG).show();
     }
 
     /**
@@ -72,17 +76,24 @@ public class MainActivity extends AppCompatActivity {
      */
     public void createYourOwn(View v) {
         Toast.makeText(this,
-                "TODO: Create Your Own Implicit Intent",
+                "COMPLETED: Create Your Own Implicit Intent",
                 Toast.LENGTH_SHORT)
                 .show();
     }
 
-    // TODO (1) Create a method called openWebPage that accepts a String as a parameter
+    // COMPLETED (1) Create a method called openWebPage that accepts a String as a parameter
+    public void openWebPage(String url){
+        Uri uri= Uri.parse(url);
+        Intent send = new Intent(Intent.ACTION_VIEW,uri);
+        if(uri!=null){
+            startActivity(send);
+        }
+    }
     // Do steps 2 - 4 within openWebPage
 
-        // TODO (2) Use Uri.parse to parse the String into a Uri
+        // COMPLETED (2) Use Uri.parse to parse the String into a Uri
 
-        // TODO (3) Create an Intent with Intent.ACTION_VIEW and the webpage Uri as parameters
+        // COMPLETED (3) Create an Intent with Intent.ACTION_VIEW and the webpage Uri as parameters
 
-        // TODO (4) Verify that this Intent can be launched and then call startActivity
+        // COMPLETED (4) Verify that this Intent can be launched and then call startActivity
 }
